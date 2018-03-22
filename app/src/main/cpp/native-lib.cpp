679edb3ea7bafb3925df44297cdb31736f0a3c7c
amogh112 <amogh112@gmail.com>
2018-03-21 21:09:50 -0400

    first commit, blog part 1 completed can include opencv

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,17 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_com_example_amogh_opencvtry1_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
