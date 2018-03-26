fd8c1b0fab2fbee4675382147817207b5faf4090
Amogh Gupta <amogh112@gmail.com>
2018-03-25 22:33:01 -0400

    face landmarks code atleast compiling fine

--- app/src/main/java/com/example/amogh/opencvtry1/OpenCVCamera.java
+++ app/src/main/java/com/example/amogh/opencvtry1/OpenCVCamera.java
@@ -1,21 +1,35 @@
 package com.example.amogh.opencvtry1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.graphics.Rect;
+import android.graphics.Bitmap;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvException;
 import org.opencv.core.Mat;
+import org.opencv.android.Utils;
+import org.opencv.imgproc.Imgproc;
+import com.tzutalin.dlib.Constants;  //amogh added for dlib
+import com.tzutalin.dlib.FaceDet; //amogh added for dlib
+import com.tzutalin.dlib.VisionDetRet; //amogh added for dlib
 
-public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+import java.util.List;
+
+
+public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "OpenCVCamera";
     private CameraBridgeViewBase cameraBridgeViewBase;
+    private Context mContext;
+    private FaceDet mFaceDet;
 
     private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -39,10 +53,12 @@ public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewB
         cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.camera_view);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);//need to see
+        mFaceDet = new FaceDet(Constants.getFaceShapeModelPath());
+        mContext = getApplicationContext();
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
@@ -65,6 +81,32 @@ public class OpenCVCamera extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+
+        Mat tmp = inputFrame.rgba();
+        Bitmap bmp = null;
+        try {
+            //Imgproc.cvtColor(seedsImage, tmp, Imgproc.COLOR_RGB2BGRA);
+//            Imgproc.cvtColor(seedsImage, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
+            bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(tmp, bmp);
+        } catch (CvException e) {
+            Log.d("Exception", e.getMessage());
+        }
+        List<VisionDetRet> results;
+        synchronized (OpenCVCamera.this) {
+            results = mFaceDet.detect(bmp);
+        }
+//        long endTime = System.currentTimeMillis();
+//        if (results != null) {
+//            for (final VisionDetRet ret : results) {
+//                float resizeRatio = 1.0f;
+//                Rect bounds = new Rect();
+//                bounds.left = (int) (ret.getLeft() * resizeRatio);
+//                bounds.top = (int) (ret.getTop() * resizeRatio);
+//                bounds.right = (int) (ret.getRight() * resizeRatio);
+//                bounds.bottom = (int) (ret.getBottom() * resizeRatio);
+//            }
+//        }
+        return tmp;
     }
 }
